eebf443a79dfe1e629c3301e4945a6916191dba1
ruizhao13 <zr1998@mail.ustc.edu.cn>
2018-06-03 09:44:19 +0800

    second commit

---
+++ lab7.srcs/sources_1/new/REG_FILE.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/03/2018 09:40:05 AM
+// Design Name: 
+// Module Name: REG_FILE
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module REG_FILE(
+
+    );
+endmodule
